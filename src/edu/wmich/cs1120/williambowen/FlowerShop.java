b6cd1327bbfa209244e853ae991001f9a6acca37
WilliamMiceli <9000802+WilliamMiceli@users.noreply.github.com>
2018-03-23 10:19:25 -0400

    Method signatures only

--- src/edu/wmich/cs1120/williambowen/FlowerShop.java
+++ src/edu/wmich/cs1120/williambowen/FlowerShop.java
@@ -3,7 +3,24 @@ package edu.wmich.cs1120.williambowen;
 public class FlowerShop {
 
 	public static void main(String[] args) {
-
+		User sUser = new User();
 	}
-
+	public static void login() {
+		
+	}
+	public static void signUp() {
+		
+	}
+	public static User checkSignUp(String userName, String password1, String password2) {
+		
+	}
+	public static void mainMenu() {
+		
+	}
+	public static void printFlowersList() {
+		
+	}
+	public static void myCart() {
+		
+	}
 }
