c8fc93abf1104d2334968cdd5aae93de2a9ffbf3
WilliamMiceli <9000802+WilliamMiceli@users.noreply.github.com>
2018-03-23 17:51:51 -0400

    Signup method is complete

--- src/edu/wmich/cs1120/williambowen/FlowerShop.java
+++ src/edu/wmich/cs1120/williambowen/FlowerShop.java
@@ -14,10 +14,11 @@ public class FlowerShop {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		User sUser = null;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to our Flowers Shop!");
 		
-		mainMenu(input);
+		mainMenu(input, sUser);
 	}
 	/**
 	 * Catch an appropriate type of exception
@@ -30,6 +31,44 @@ public class FlowerShop {
 	 */
 	public static void signUp(Scanner input) {
 		
+		String userName;
+		String password1;
+		String password2;
+		
+		boolean userNameUnique = true;
+		boolean passwordsMatch = true;
+		boolean passwordIsValid = true;
+		
+		do {
+			do {
+				System.out.println("Enter your UserName:");
+				userName = input.nextLine();
+				System.out.println("Enter your Password:");
+				password1 = input.nextLine();
+				System.out.println("Enter your Password again:");
+				password2 = input.nextLine();
+				
+				userNameUnique = true;
+				for(int i = 0; i < Lookup.userList.length; ++i) {
+					if(Lookup.userList[i].userName.equals(userName)) {
+						userNameUnique = false;
+						System.out.println("UserName already exists.");
+						break;
+					}
+				}
+				
+				passwordsMatch = true;
+				if(password1.compareTo(password2) != 0) {
+					passwordsMatch = false;
+					System.out.println("Passwords do not match.");
+				}
+			}while(userNameUnique && passwordsMatch);
+			
+			passwordIsValid = true;
+			if(Lookup.isValidPassword(password1).compareTo("error") == 0) {
+				passwordIsValid = false;
+			}
+		}while(passwordIsValid);
 	}
 	/**
 	 * Catch an appropriate type of exception
@@ -44,7 +83,7 @@ public class FlowerShop {
 	/**
 	 * 
 	 */
-	public static void mainMenu(Scanner input) {
+	public static void mainMenu(Scanner input, User sUser) {
 		System.out.println("1- SignUp");
 		System.out.println("2- Login");
 		System.out.println("3- Exit");
