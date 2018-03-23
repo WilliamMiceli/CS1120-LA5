cb07235046b75087573b70c8388102740a0ad2ba
WilliamMiceli <9000802+WilliamMiceli@users.noreply.github.com>
2018-03-23 10:25:04 -0400

    Method signatures only

---
+++ src/edu/wmich/cs1120/williambowen/Lookup.java
@@ -0,0 +1,25 @@
+package edu.wmich.cs1120.williambowen;
+
+public class Lookup {
+	User[] userList = new User[];
+	Item[] storeItemList = new Item[];
+	
+	public static User checkLoginAuth(String userName, String password) {
+		
+	}
+	public static User checkSignUpAuth(String userName, String password1, String password2) {
+		
+	}
+	public static User[] createUsers() {
+		
+	}
+	public static Item[] loadItems() {
+		
+	}
+	public static Item getItemById(int key) {
+		
+	}
+	public static String isValidPassword(String password) {
+		
+	}
+}
